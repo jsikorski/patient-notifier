db8af861f60162f2d9c7a4dc38d5b40b321020cc
Jakub Sikorski <jakubsikorski7@gmail.com>
2014-06-17 22:11:19 +0200

    chore: travis signal

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.agenda = require('./lib/notifications/scheduler');
 
 // Start server
 app.listen(config.port, function () {
-  console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
+  console.log('Express server listening on port %d in %s mode', config.port, app.get('env')); 
 });
 
 // Expose app
